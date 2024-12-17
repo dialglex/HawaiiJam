7aaae5722fad49ebe79f4207a9a4767f1cb264ef
Alex Kent <dialglex@gmail.com>
2024-12-17 17:53:01 +1100

    Added death particles

--- scenes/entity/player/player.gd
+++ scenes/entity/player/player.gd
@@ -2,6 +2,7 @@ class_name Player
 extends CharacterBody2D
 
 
+var death_particles_scene := preload("res://scenes/particle/death_particles.tscn")
 var starting_position: Vector2
 
 
@@ -15,4 +16,8 @@ func _on_door_area_area_entered(_area: Area2D) -> void:
 
 
 func _on_enemy_area_area_entered(area: Area2D) -> void:
+	var death_particles := death_particles_scene.instantiate()
+	get_tree().root.get_child(0).add_child(death_particles)
+	death_particles.position = position + Vector2(0, -32)
+	death_particles.emitting = true
 	position = starting_position
